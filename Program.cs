15bbec148541723d6209d550eb7a64a851ae772b
YuriyP <solovej-1991@mail.ru>
2022-06-24 00:24:26 +0600

    Теперь число можно ввести в консоли

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
-﻿int number_1 = 5;
+﻿Console.Write("Input number: ");
+int number_1 = Convert.ToInt32(Console.ReadLine()); //преобразовывает из консоли в целое число
 
 Console.WriteLine(number_1 + "->" + number_1*number_1);
