ea8687b656a7c28b5b1c38749ce6eba2cfa91dc8
YuriyP <solovej-1991@mail.ru>
2022-06-24 00:27:15 +0600

    Внес некоторые коментарии

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Input number: ");
+﻿Console.Write("Input number: "); // запрашиваем пользователя число
 int number_1 = Convert.ToInt32(Console.ReadLine()); //преобразовывает из консоли в целое число
 
-Console.WriteLine(number_1 + "->" + number_1*number_1);
+Console.WriteLine(number_1 + "->" + number_1*number_1); //производим всю операцию по возведению числа в квадрат
